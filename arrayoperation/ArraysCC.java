d4791f086e677d1ccab148a2a2458df5a3a953f9
gautam yadav <gy77044@gmail.com>
2023-12-05 22:06:29 +0530

    arrays

--- arrayoperation/ArraysCC.java
+++ arrayoperation/ArraysCC.java
@@ -1,16 +1,35 @@
+// import java.util.*;
+
+// public class ArraysCC {
+//   public static void main(String args[]) {
+//     int marks[] = new int[100];
+//     Scanner sc = new Scanner(System.in);
+//     marks[0] = sc.nextInt();
+//     marks[1] = sc.nextInt();
+//     marks[2] = sc.nextInt();
+
+//     System.out.println("phy : " + marks[0]);
+//     System.out.println("chem : " + marks[1]);
+//     System.out.println("math : " + marks[2]);
+//   }
+// }
+
 import java.util.*;
 
 public class ArraysCC {
+  public static void update(int marks[]) {
+    for (int i = 0; i < marks.length; i++) {
+      marks[i] = marks[i] + 1;
+    }
+  }
+
   public static void main(String args[]) {
-    int marks[] = new int[100];
-    Scanner sc = new Scanner(System.in);
+    int marks[] = { 97, 98, 99 };
+    update(marks);
 
-    marks[0] = sc.nextInt(); // Physics
-    marks[1] = sc.nextInt(); // Chemistry
-    marks[2] = sc.nextInt(); // Maths
-
-    System.out.println("Physics :" + marks[0]);
-    System.out.println("Chemisty :" + marks[1]);
-    System.out.println("Maths :" + marks[2]);
+    for (int i = 0; i < marks.length; i++) {
+      System.out.print(marks[i] + " ");
+    }
+    System.out.println();
   }
 }
